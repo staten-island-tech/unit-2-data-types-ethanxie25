70906c8236c8e0e1d045d9c723d6e389620d5085
ethanxie25 <randomized0938@gmail.com>
2024-09-16 09:20:19 -0400

    data types

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,6 +1,8 @@
+from math import remainder
+
 x = int
 odd_or_even = input('odd or even')
-if odd_or_even == int%2:
+if odd_or_even == int%2 and remainder:
     print('correct')
 else:
     print('incorrect')
---
+++ tipcalc.py
@@ -0,0 +1,9 @@
+bill = 0
+tip = 0
+tap = 0
+
+bill = float(input('kyrie'))
+tip = float(input('ben'))
+tap = bill + tip
+
+print(tap)
