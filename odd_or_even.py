39119f6625e6565f11e3a1ca2c65c71aed189182
ethanxie25 <randomized0938@gmail.com>
2024-09-17 09:20:15 -0400

    odd or even

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,8 +1,10 @@
 from math import remainder
+y = remainder
 
 x = int
-odd_or_even = input('odd or even')
-if odd_or_even == int%2 and remainder:
-    print('correct')
+odd_or_even = input("is it odd or even")
+if odd_or_even == ('2', '4', '6', '8', '10', '12', '14', '16', '18', '20'):
+    print('even')
 else:
-    print('incorrect')
+    print('odd')
+ 
