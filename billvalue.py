147fd49371d85699775887770aaed92661215c5d
ethanxie25 <randomized0938@gmail.com>
2024-09-18 09:20:37 -0400

    bill value odd or even

---
+++ billvalue.py
@@ -0,0 +1,12 @@
+service = 0
+bill = 0
+tap = 0
+
+if service == 'great':
+    print('25%')
+elif service == 'good':
+    print('20%')
+elif service == 'okay':
+    print('15%')
+elif service == 'bad':
+    print('0%')
---
+++ oddoreven.py
@@ -0,0 +1,5 @@
+odd_or_even = int(input('Is it odd or even?'))
+if odd_or_even % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
