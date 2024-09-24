0af7b90ed3852bde28e9045402ce04a7f9eb930a
ethanxie25 <randomized0938@gmail.com>
2024-09-24 09:22:00 -0400

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#madlibs
+x = input('On a regular day, John went outside for a __. Type in a verb.')
+print(f'On a regular day, John went outside for a {x}.')
+y = input('While he was running, he was not looking and fell into the water for a __. Type in a verb.')
+print(f'While he was running, he was not looking and fell into the water for a {y}.')
+z = input('Later, he was rescued by __ and went on his yacht. Type in a celebrity.')
+print(f'Later, he was rescued by {z} and went on his yacht.')
+a = input('When he got back to land, he started to __ again. Type in the first verb.')
+print(f'When he got back to land, he started to {a} again.')
